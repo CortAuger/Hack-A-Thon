cdff5adc04eb05e7e5149b03eb05ff87a887e14b
hmarchen <156877222+hmarchen@users.noreply.github.com>
2025-03-23 09:54:33 -0400

    mobile button removed

--- drtime/src/components/Navigation.tsx
+++ drtime/src/components/Navigation.tsx
@@ -93,50 +93,6 @@ export default function Navigation() {
             DRTIME
           </Typography>
 
-          {/* Mobile menu */}
-          {/*
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-             <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  key={page.name}
-                  onClick={() => {
-                    handleCloseNavMenu();
-                    router.push(page.path);
-                  }}
-                >
-                  <Typography textAlign="center">{page.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> 
-          </Box> */}
 
           {/* Logo for mobile view */}
           <Box sx={{ 
